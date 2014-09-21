ae9c88ea8c6b2166eb874509f57b655c3f4d54a9
Yaxuan Zhang <yxzhang@CNyxzhang.local>
2014-09-21 11:39:19 +0800

    admin login

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140919080705) do
+
+  create_table "users", force: true do |t|
+    t.string   "login"
+    t.string   "hashed_password"
+    t.string   "salt"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
