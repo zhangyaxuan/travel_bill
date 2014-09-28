572021826c295312527770d6804d77cbf59185e7
Yaxuan Zhang <yxzhang@CNyxzhang.local>
2014-09-28 20:05:16 +0800

    add members when creating a new travel

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -2,9 +2,18 @@ class TravelsController < ApplicationController
 
 	def new
 		@travel = Travel.new
+		@users = User.all
 	end
 	def create
 		@travel = Travel.new(travel_params)
+
+		#binding.pry
+		user_ids = params['user_ids']
+		user_ids.each do |u_id|
+			u = ::User.find(u_id)
+			@travel.users << u
+		end
+
 		@travel.creater = User.find(session[:user_id])
 		if @travel.save
 			redirect_to travels_path
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,4 +3,7 @@ class Bill < ActiveRecord::Base
 	has_many :costs
 	has_many :users, :through => :costs
  	belongs_to :user, foreign_key: 'payer_id'
+
+ 	def payer
+ 	end
 end
