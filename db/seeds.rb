69ee721c378c4b47ff918418f1a121f7aca8cf82
Yaxuan Zhang <yxzhang@cnshzheng.corporate.thoughtworks.com>
2014-10-11 16:58:32 +0800

    clear Procfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create({login: 'nancy', password: '111'}, {login: 'sean', password: '111'}, {login: 'rocky', password: '111'}) if User.where(login:'nancy').blank?
+users = User.create({login: 'nancy', password: '111'}, {login: 'sean', password: '111'}, {login: 'rocky', password: '111'}) if User.where(login:'nancy').blank?
