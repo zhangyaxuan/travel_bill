aa80622dd7fbe9903892594b2ddc70a07b53b9fc
Yaxuan Zhang <yxzhang@CNyxzhang.local>
2014-10-12 18:46:56 +0800

    calculate for every member, and show the result

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -32,12 +32,26 @@ class TravelsController < ApplicationController
 
 	def result
 		@travel = Travel.find(params[:travel_id])
-		@total_cost = @travel.total_cost
-		@results = Cost.cost_by_user(@travel.bills).map{|x| {x.user_id => x.user_money}}
+		@travel_cost = @travel.total_cost
+		@user_costs = Cost.cost_by_user(@travel.bills).map{|x| {x.user_id => x.user_money}}
+
+		@user_pays = Bill.pay_by_user(@travel.bills).map { |x| {x.payer_id => x.user_pay}  }
+		
+		@results = @user_costs.map{|x| {x.keys.first => calculate(x, @user_pays)} }
 	end
+
 
 	private
 	def travel_params
 		params.require(:travel).permit(:name)
+	end
+
+	def calculate(user_cost, user_pays)
+		cost = user_cost.values.first
+		user_pay = @user_pays.select {|y| user_cost.keys.first == y.keys.first }.at(0)
+		unless user_pay.blank?
+		 	return user_pay.values.first - cost
+		end 
+		return 0 - cost
 	end
 end
