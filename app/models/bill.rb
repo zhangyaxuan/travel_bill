ae187b65c8f599acd6ecf19fab3f12af2018c829
Yaxuan Zhang <yxzhang@CNyxzhang.local>
2014-10-12 18:46:56 +0800

    calculate for every member, and show the result

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -4,6 +4,6 @@ class Bill < ActiveRecord::Base
 	has_many :users, :through => :costs
  	belongs_to :user, foreign_key: 'payer_id'
 
- 	def payer
- 	end
+ 	scope :pay_by_user, ->(bills){ bills.select('sum(cost) as user_pay, payer_id').group('payer_id')}
+
 end
