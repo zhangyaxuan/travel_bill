2d99e1cce24ff4045869ae2c02aff68d92804647
Yaxuan Zhang <yxzhang@cnshzheng.corporate.thoughtworks.com>
2014-10-13 18:08:14 +0800

    extract checkbox logic from html to helper; click bill link, then go to the edit bill page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	def select_users(users)
+		html = ""
+		users.each_with_index do |user, index|
+			html += "<label>"
+			html += check_box_tag "user_ids[]", user.id, nil, {id: "user_ids_#{index}"}
+			html += user.login
+			html += "</label>"
+		end
+		html.html_safe
+	end
 end
