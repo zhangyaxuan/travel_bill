5ca8aac7ea7c297d5bd2e8bd868427696fa72157
Yaxuan Zhang <yxzhang@cnshzheng.corporate.thoughtworks.com>
2014-10-14 17:57:14 +0800

    fix problems after merge

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,13 @@ module ApplicationHelper
 	def select_users(users)
 		html = ""
 		users.each_with_index do |user, index|
-			html += "<label>"
+			html += '<label class="form-control">'
 			html += check_box_tag "user_ids[]", user.id, nil, {id: "user_ids_#{index}"}
 			html += user.login
-			html += "</label>"
+			html += '</label>'
 		end
 		html.html_safe
 	end
+
+
 end
